074e2c7a1be52410d5b0b8ae8777f7f59dd8cf48
Sergio Ceron <sergio@blerify.com>
2026-04-05 20:07:45 -0500

    fix: Implement Name Validation in Blueprint Initialization

--- tests/test_blueprints.py
+++ tests/test_blueprints.py
@@ -256,6 +256,10 @@ def test_dotted_name_not_allowed(app, client):
         flask.Blueprint("app.ui", __name__)
 
 
+def test_empty_name_not_allowed(app, client):
+    with pytest.raises(ValueError, match="'name' may not be an empty string."):
+        flask.Blueprint('', __name__)
+
 def test_dotted_names_from_app(app, client):
     test = flask.Blueprint("test", __name__)
 

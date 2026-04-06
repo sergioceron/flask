289994cf8536ddb8ef8a14a3a1facfc4993e32d9
Sergio Ceron <sergio@blerify.com>
2026-04-05 20:07:12 -0500

    fix: Update Examples and Tests with Valid Blueprint Names

--- tests/test_apps/blueprintapp/__init__.py
+++ tests/test_apps/blueprintapp/__init__.py
@@ -2,8 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
-from blueprintapp.apps.admin import admin
-from blueprintapp.apps.frontend import frontend
+from blueprintapp.apps.admin import admin_bp
+from blueprintapp.apps.frontend import frontend_bp
 
-app.register_blueprint(admin)
-app.register_blueprint(frontend)
+app.register_blueprint(admin_bp)
+app.register_blueprint(frontend_bp)
